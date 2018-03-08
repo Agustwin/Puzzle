ccd2eafd3080303b67aed7df1d08276c15a2aa28
Agus <agustwin@gmail.com>
2018-03-08 19:19:40 +0100

    Puzzle se pinta

---
+++ PuzzleFase1Stu/src/control/Controller.java
@@ -0,0 +1,19 @@
+package control;
+
+import java.awt.event.ActionEvent;
+
+public class Controller extends AbstractController{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyObservers(int blankPos, int movedPos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
