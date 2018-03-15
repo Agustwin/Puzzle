7627cf119c334259b60a703797c11e8b4fa2ed3f
lalinlulelo <lalinlulelo@gmail.com>
2018-03-15 01:41:16 +0100

    Jdom2

--- PuzzleFase1Stu/src/PuzzleApp.java
+++ PuzzleFase1Stu/src/PuzzleApp.java
@@ -8,10 +8,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 import control.AbstractController;
 import control.Controller;
