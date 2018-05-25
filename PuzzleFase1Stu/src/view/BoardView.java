d8f3f5b8a39ec1f23cf86c1e078e1e30bc717966
Agus <agustwin@gmail.com>
2018-05-25 09:54:21 +0200

    Revert "Se supone que ya no pinta todo el tablero cada vez"
    This reverts commit a4b007f204524a23d43d08dcacbad927ff4a92db.

--- PuzzleFase1Stu/src/view/BoardView.java
+++ PuzzleFase1Stu/src/view/BoardView.java
@@ -31,10 +31,6 @@ import java.util.List;
  * @version 1.0
  */
 public class BoardView extends JPanel implements Observer {
-	
-   public static final int windowWidth=244;
-   public static final int windowHeight=162;
-	
     public static final int imageWidth= 96;
     public static final int imageHeight= 96;
     private ArrayList<PieceView> iconArray = null;
@@ -58,11 +54,9 @@ public class BoardView extends JPanel implements Observer {
         for(int i=0;i<rowNum*columnNum;i++) {      		
         		PieceView p=new PieceView( i,i%rowNum,i/rowNum,imageSize,imageList[i]);
     		
-        		System.err.println("INDEXCOLUMN "+p.getIndexColumn()+"INDEXROW "+p.getIndexRow());
-        		SetDrawnCoordinates(p,imageSize);
         		iconArray.add(p);
             	
-            	
+            	SetDrawnCoordinates(p,imageSize);
         }
     }
 
@@ -84,9 +78,7 @@ public class BoardView extends JPanel implements Observer {
         		 p=new PieceView( i,i%rowNum,i/rowNum,imageSize,listImg[i]); 
         		
         	}
-    		System.err.println("INDEXCOLUMN "+p.getIndexColumn()+"INDEXROW "+p.getIndexRow());
-
-        	SetDrawnCoordinates(p,imageSize);
+    	
         	iconArray.add(p);
         	
         }        
@@ -127,7 +119,7 @@ public class BoardView extends JPanel implements Observer {
     }
 
     public void update(int blankPos, int movedPos){
-
+    	    	
     	PieceView blank=iconArray.get(blankPos);
 
     	iconArray.set(blankPos,iconArray.get(movedPos));
@@ -144,16 +136,14 @@ public class BoardView extends JPanel implements Observer {
 		iconArray.get(blankPos).setIndexRow(auxX);
     	
     	
-    	//Actualizo las coordenadas en pantalla de las piezas movidas
-    	
-    		SetDrawnCoordinates(iconArray.get(movedPos),iconArray.get(movedPos).getImageSize());
-    		SetDrawnCoordinates(iconArray.get(blankPos),iconArray.get(blankPos).getImageSize());
-
-    	
+    	//Actualizo las coordenadas de todo el puzle
+    	for(int i=0;i<iconArray.size();i++) {
+    		SetDrawnCoordinates(iconArray.get(i),iconArray.get(i).getImageSize());
+    	}
     	
     	System.out.println("View: ");
 		for(PieceView p:iconArray) {
-			//System.out.println("id: "+p.getId()+" X: "+p.getIndexRow()+" Y: "+p.getIndexColumn());
+			System.out.println("id: "+p.getId()+" X: "+p.getIndexRow()+" Y: "+p.getIndexColumn());
 		}
     	
 		update(getGraphics());
@@ -166,10 +156,12 @@ public class BoardView extends JPanel implements Observer {
 
     public void paint(Graphics g){
     		
-    	//limpio antes de pintar
-    	g.clearRect(0, 0,windowWidth, windowHeight);
-    	//pinto la pantalla
+    	//g.clearRect(0, 0,this.getWidth(), this.getHeight());
+
 		for(PieceView iconImage:iconArray){	
+    			
+			//Asigno las coordenadas en las que se va a pintar
+    		SetDrawnCoordinates(iconImage,iconImage.getImageSize());
     		
             g.drawImage(iconImage.getImage(), iconImage.getDrawnRowIndex(), iconImage.getDrawnColumnIndex(), iconImage.getImageSize(), iconImage.getImageSize(), this);
             
@@ -230,7 +222,7 @@ public class BoardView extends JPanel implements Observer {
     
 	//Comprueba si el movimiento es valido devuelve los ids 
 	public int checkMove(int pos) {
-
+	
 		int blankPos=-1;
 		 rowNum=imageWidth/iconArray.get(0).getImageSize();
 		
@@ -269,7 +261,7 @@ public class BoardView extends JPanel implements Observer {
 
 
 	public void Clutter() {		
-		Collections.shuffle(iconArray);	
+		Collections.shuffle(iconArray);			
 	}
 		
 	@Override
@@ -310,14 +302,11 @@ public class BoardView extends JPanel implements Observer {
 
 	private void SetDrawnCoordinates(PieceView p,int imageSize) {
 	
-
-		rowOff=(windowWidth-this.imageWidth)/2;
-		colOff=(windowHeight-this.imageHeight)/2;
+		rowOff=(this.getWidth()-this.imageWidth)/2;
+		colOff=(this.getHeight()-this.imageHeight)/2;
 		
 		int drawnRow;
 		int drawnColumn;
-		
-		
 		
 		 drawnRow=p.getIndexRow()*imageSize;
 		 drawnColumn=p.getIndexColumn()*imageSize;    	 
